d44fcbadac63077464242d9e2971b234dabf6cc3
Owchachi <michnom179@gmail.com>
2021-03-14 16:54:08 +0100

    designing orders tab

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,35 @@
 import {Carousel} from './common/Carousel';
+import {useState, useEffect} from 'react';
 
 export const Order = () => {
+
+    const [currentActive, setCurrentActive] = useState(null);
+    const foodTypes = ['Sushi rolls', 'Ramen', 'Prawns', 'Sets'];
+
+    useEffect(() =>{
+        const allHeaders = document.querySelectorAll('.foodHeadersContainer h1');
+        allHeaders.forEach(element => element.classList.add('notActive'));
+        allHeaders[0].classList.add('active');
+        setCurrentActive(allHeaders[0].innerHTML);
+    }, [])
+
+    const changeActive = (e) => {
+        const allHeaders = document.querySelectorAll('.foodHeadersContainer h1');
+        allHeaders.forEach(element => element.classList.remove('active'));
+        e.target.classList.add('active');
+        setCurrentActive(e.target.innerHTML);
+    }
+
+    useEffect(() =>{
+        console.log(currentActive);
+    }, [currentActive])
+
     return(
         <div className="orderMainContainer">
             <div className="foodHeadersContainer">
-                <h1>Sushi rolls</h1><h1>Ramen</h1><h1>Prawns</h1><h1>Sets</h1>
+                {foodTypes.map(value => {
+                    return <h1 onClick={changeActive}>{value}</h1>
+                })}
             </div>
             <div className="carouselContainer">
                 <Carousel/>
--- src/components/common/Carousel.js
+++ src/components/common/Carousel.js
@@ -6,8 +6,8 @@ export const Carousel = () =>{
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 5
+        slidesToShow: 3,
+        slidesToScroll: 3
       };
 
       return (
@@ -17,30 +17,15 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-            </div>
-            <div className="sliderElement">
-            <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
@@ -49,30 +34,15 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-            </div>
-            <div className="sliderElement">
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
@@ -81,30 +51,15 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-            </div>
-            <div className="sliderElement">
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
@@ -113,30 +68,15 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-            </div>
-            <div className="sliderElement">
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
@@ -145,14 +85,15 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
-                </div>
-              </div>
-              <div>
-                <div className="productImage">
-                </div>
-                <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
@@ -161,14 +102,83 @@ export const Carousel = () =>{
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
+            </div>
+            <div className="sliderElement">
               <div>
                 <div className="productImage">
                 </div>
                 <div className="productDetails">
-                  <h3>Philadelphia 6.00$</h3>
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
+                </div>
+              </div>
+            </div>
+            <div className="sliderElement">
+              <div>
+                <div className="productImage">
+                </div>
+                <div className="productDetails">
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
+                </div>
+              </div>
+            </div>
+            <div className="sliderElement">
+              <div>
+                <div className="productImage">
+                </div>
+                <div className="productDetails">
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
+                </div>
+              </div>
+            </div>
+            <div className="sliderElement">
+              <div>
+                <div className="productImage">
+                </div>
+                <div className="productDetails">
+                  <div className="productHeader">
+                    <h2>Philadelphia roll</h2>
+                  </div>
+                  <hr></hr>
+                  <div className="productIngredients">
+                    <p>Contains: rice, nori leaf, fresh salmon, philadeplhia cream, cucumber, avocado, sesam</p>
+                    <p><b>~ 6.00$</b></p>
+                  </div>
+                  <button>Order</button>
                 </div>
               </div>
             </div>
