514ff86bdd5fad389959f8c94fa8318cd1ea8bee
unknown <michnom179@gmail.com>
2021-05-04 13:38:24 +0200

    more tests

--- src/components/landingPageComponents/productsGroups/__test__/ProductsGroups.test.js
+++ src/components/landingPageComponents/productsGroups/__test__/ProductsGroups.test.js
@@ -4,6 +4,7 @@ import mockedAxios from 'axios';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'; 
 import {render, fireEvent, waitFor} from '@testing-library/react';
+import TestRenderer from "react-test-renderer";
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 
@@ -32,10 +33,10 @@ test("check if other product groups than default are showing after onClick event
     })
 });
 
-test("check if produdct group is changed after click on other group from dropdon", async() => {
+test("check if produdct group is changed after click on other group from dropdown", async() => {
     mockedAxios.get.mockResolvedValueOnce({data: validProductGroupJson});
 
-    const {getByTestId, getByText} = render(<Provider store={store}><ProductsGroups/></Provider>);
+    const {getByTestId} = render(<Provider store={store}><ProductsGroups/></Provider>);
 
     fireEvent.click(getByTestId('dropDown'));
 
@@ -46,4 +47,39 @@ test("check if produdct group is changed after click on other group from dropdon
     await waitFor(() =>{
         expect(getByTestId('selected').textContent).toBe("prawns");
     })
+});
+
+test("Initial render should call dispatch one time after api call", async() => {
+    mockedAxios.get.mockResolvedValueOnce({data: validProductGroupJson});
+
+    const {getByTestId} = render(<Provider store={store}><ProductsGroups/></Provider>);
+
+    store.dispatch = jest.fn();
+
+    await waitFor(() =>{
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+    })
+});
+
+test("On product group click dispatch should be called", async() => {
+    mockedAxios.get.mockResolvedValueOnce({data: validProductGroupJson});
+
+    const {getByTestId} = render(<Provider store={store}><ProductsGroups/></Provider>);
+
+    store.dispatch = jest.fn();
+
+    fireEvent.click(getByTestId('dropDown'));
+
+    await waitFor(() =>{
+        fireEvent.click(getByTestId('group1'));
+    });
+
+    await waitFor(() =>{
+        expect(store.dispatch).toHaveBeenCalledTimes(2);
+    })
+});
+
+test("matches snapshot", () => {
+    const tree = TestRenderer.create(<Provider store={store}><ProductsGroups/></Provider>).toJSON();
+    expect(tree).toMatchSnapshot();
 });
